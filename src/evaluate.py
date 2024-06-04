72b2ea736c31762af90dcec3b5afe404d1089436
Piotr Samczuk <samczukp@gmail.com>
2024-06-04 22:41:23 +0200

    Add '--filename' flag functionality to evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,13 +1,14 @@
+import os
+import argparse
 from data_loader import load_data
 from tensorflow.keras.models import load_model
-import os
 from tkinter import messagebox
 
-def evaluate_model():
+def evaluate_model(filename):
     (x_train, y_train), (x_test, y_test) = load_data()
-    model_file = 'models/mnist_model1.keras'
+    model_file = f'models/{filename}.keras'
     if not os.path.isfile(model_file):
-        messagebox.showerror("Error", "Model file not found. Please generate the model first.")
+        messagebox.showerror("Error", f"Model file '{filename}' not found. Please generate the model first.")
         return
     else:
         model = load_model(model_file)
@@ -16,4 +17,8 @@ def evaluate_model():
     print(f"Test accuracy: {test_acc}")
 
 if __name__ == "__main__":
-    evaluate_model()
+    parser = argparse.ArgumentParser(description='Evaluate a model on MNIST dataset.')
+    parser.add_argument('--filename', type=str, default='mnist_model', help='Name of the model file to be evaluated.')
+    args = parser.parse_args()
+
+    evaluate_model(args.filename)
